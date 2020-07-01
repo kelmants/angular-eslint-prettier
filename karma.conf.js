55aae5c1e184bf57bab3ca142b679d8bda36d40a
kelman celis <kelmancelis@icloud.com>
2020-07-01 11:57:31 -0300

    initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,10 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-eslint-prettier'),
+      dir: require('path').join(
+        __dirname,
+        './coverage/angular-eslint-prettier'
+      ),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
